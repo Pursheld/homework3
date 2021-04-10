c516703c22939a7cbb67e0fa8c83b8568d923b3d
Pursheld <stompee@bk.ru>
2021-04-10 16:53:30 +0300

    Добавьте файлы проекта.

---
+++ homework3.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+int main()
+{
+    int z = 10;
+    for (int i = 1; i <= z; ++i)
+    {
+        if (i % 2 == 0)
+        {
+            std::cout << i;
+        }
+
+    }
+    std::cout << "\n";
+    for (int i = 1; i <= z; i++)
+    {
+        if (i % 3 == 0)
+        {
+            std::cout << i;
+        }
+    }
+}
+
